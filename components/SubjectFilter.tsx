175db39e2d6567d914d126457daa0a1b69416425
Rhoda Eboreime <rhodaivioje@gmail.com>
2025-06-03 12:40:39 +0100

    feat: enhance CompanionsLibrary with search and filter functionality; add SearchInput and SubjectFilter components

---
+++ components/SubjectFilter.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import React, { useEffect, useState } from 'react'
+import { formUrlQuery, removeKeysFromUrlQuery } from '@jsmastery/utils';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select';
+import { subjects } from '@/constants';
+
+const SubjectFilter = () => {
+        const router = useRouter();
+        const searchParams = useSearchParams();
+    
+        const query = searchParams.get('subject') || "";
+        const [subject, setSubject] = useState(query);
+        
+        useEffect(() => {
+            let newUrl = "";
+            if(subject === 'all') {
+                newUrl = removeKeysFromUrlQuery({
+                    params: searchParams.toString(),
+                    keysToRemove: ["subject"],
+                });
+            } else {
+                newUrl = formUrlQuery({
+                    params: searchParams.toString(),
+                    key: "subject",
+                    value: subject
+                });
+                router.push(newUrl, {scroll: false});
+            }
+            router.push(newUrl, {scroll: false});
+        }, [subject])
+
+  return (
+    <Select onValueChange={setSubject} value={subject}>
+        <SelectTrigger className="input capitalize">
+            <SelectValue placeholder="subject" />
+        </SelectTrigger>
+        <SelectContent>
+            <SelectItem value="all">All Subjects</SelectItem>
+            {subjects.map((subject) => (
+            <SelectItem key={subject} value={subject} className='capitalize'>
+                {subject}
+            </SelectItem>
+            ))}
+        </SelectContent>
+    </Select>
+  )
+}
+
+export default SubjectFilter
