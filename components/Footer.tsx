29027a149b2465e09460c1bdf39b673ca830cbb6
Rhoda Eboreime <rhodaivioje@gmail.com>
2025-07-02 15:42:09 +0100

    fix: remove unnecessary whitespace in Footer component for cleaner code

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 const Footer = () => {
   const currentYear = new Date().getFullYear()
 
+  
   return (
     <footer className="bg-gray-900 text-gray-300 pt-10 pb-6 mt-12">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
